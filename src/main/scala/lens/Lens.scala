28548e594ebd154fc966949b47195bd524c362ee
William Haw <me@williamhaw.com>
2019-08-08 22:00:36 +0800

    Add compose function for lenses and example of composition

--- src/main/scala/lens/Lens.scala
+++ src/main/scala/lens/Lens.scala
@@ -1,3 +1,13 @@
 package lens
 
 case class Lens[A, B](get: A => B, set: (A, B) => A)
+
+object LensUtils {
+  def compose[Outer, Inner, Value](
+      outer: Lens[Outer, Inner],
+      inner: Lens[Inner, Value]): Lens[Outer, Value] =
+    Lens[Outer, Value](
+      get = outer.get andThen inner.get,
+      set = (obj, value) => outer.set(obj, inner.set(outer.get(obj, value)))
+    )
+}
--- src/test/scala/lens/LensSpec.scala
+++ src/test/scala/lens/LensSpec.scala
@@ -1,11 +1,14 @@
 package lens
 
-import models.Street
+import models.{Address, Street}
 import org.scalatest.{FlatSpec, Matchers}
+import lens.LensUtils._
 
 class LensSpec extends FlatSpec with Matchers{
 
-  val streetNumberLens = Lens[Street, Int](get = _.number, set = (a,b) => a.copy(number = b))
+  val streetNumberLens: Lens[Street, Int] = Lens[Street, Int](get = _.number, set = (a, b) => a.copy(number = b))
+  val addressStreetLens: Lens[Address, Street] = Lens[Address, Street](get = _.street, set = (a, b) => a.copy(street = b))
+  val addressStreetNumberLens = compose(addressStreetLens, streetNumberLens)
 
   "The Lens" should "get value of attribute in case class" in {
     val street = Street("Long Avenue", 3)
