593f600981725d520025c34a1cf3106e48027772
William Haw <me@williamhaw.com>
2019-08-08 22:15:20 +0800

    Move lenses into Account.scala file

--- src/main/scala/models/Account.scala
+++ src/main/scala/models/Account.scala
@@ -1,9 +1,18 @@
 package models
 
-case class Account()
+import lens.Lens
+import lens.LensUtils.compose
+
+case class Account(id: Long, user: User, isActive: Boolean)
 
 case class User(id: Long, address: Address)
 
 case class Address(country: String, city: String, street: Street)
 
 case class Street(name: String, number: Int)
+
+object AccountLenses{
+  val streetNumberLens: Lens[Street, Int] = Lens[Street, Int](get = _.number, set = (a, b) => a.copy(number = b))
+  val addressStreetLens: Lens[Address, Street] = Lens[Address, Street](get = _.street, set = (a, b) => a.copy(street = b))
+  val addressStreetNumberLens: Lens[Address, Int] = compose(addressStreetLens, streetNumberLens)
+}
--- src/test/scala/lens/LensSpec.scala
+++ src/test/scala/lens/LensSpec.scala
@@ -2,22 +2,33 @@ package lens
 
 import models.{Address, Street}
 import org.scalatest.{FlatSpec, Matchers}
-import lens.LensUtils._
+import models.AccountLenses._
 
 class LensSpec extends FlatSpec with Matchers{
-
-  val streetNumberLens: Lens[Street, Int] = Lens[Street, Int](get = _.number, set = (a, b) => a.copy(number = b))
-  val addressStreetLens: Lens[Address, Street] = Lens[Address, Street](get = _.street, set = (a, b) => a.copy(street = b))
-  val addressStreetNumberLens = compose(addressStreetLens, streetNumberLens)
 
   "The Lens" should "get number of street from Street case class" in {
     val street = Street("Long Avenue", 3)
     streetNumberLens.get(street) shouldEqual 3
   }
 
-  "The Lens" should "set number of street in copy of Street case class" in {
+  it should "set number of street in copy of Street case class" in {
     val street = Street("Short Street", 4)
     streetNumberLens.set(street, 25).number shouldEqual 25
+  }
+
+  it should "get number of street from Address case class" in {
+    val street = Street("Left Side", 10)
+    val address = Address("USA", "New York", street)
+
+    addressStreetNumberLens.get(address) shouldEqual 10
+
+  }
+
+  it should "set number of street in copy of Address case class" in {
+    val street = Street("Left Side", 10)
+    val address = Address("USA", "New York", street)
+
+    addressStreetNumberLens.set(address, 3).street.number shouldEqual 3
   }
 
 }
